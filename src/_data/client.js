8602073323cc4eb59a4990db711103054b8a4b25
Payton Chertude <p.chertude34@gmail.com>
2024-07-16 07:04:58 -0700

    update cleint data

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,20 +1,18 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "West Coast Web Designs",
+    "email": "wcwd.info@gmail.com",
+    "phoneForTel": "503-956-2541",
+    "phoneFormatted": "(503) 956-2542",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
+        "lineOne": "4889 Tate Ave N",
+        "city": "Keizer",
+        "state": "OR",
+        "zip": "97303",
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
         "instagram": "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://www.westcoastwebdesigns.com"
 };
