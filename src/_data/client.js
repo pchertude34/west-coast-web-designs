6c951d075d24567fa04c50db35177f03ec99791f
Payton Chertude <p.chertude34@gmail.com>
2024-10-09 17:39:43 -0700

    update email

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,18 +1,18 @@
 module.exports = {
-  name: 'West Coast Web Designs',
-  email: 'wcwd.info@gmail.com',
-  phoneForTel: '503-962-0576',
-  phoneFormatted: '(503) 962-0576',
-  address: {
-    lineOne: '4889 Tate Ave N',
-    city: 'Keizer',
-    state: 'OR',
-    zip: '97303',
-  },
-  socials: {
-    facebook: 'https://www.facebook.com/',
-    instagram: 'https://www.instagram.com/',
-  },
-  //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-  domain: 'https://westcoastwebdesigns.com',
+    "name": "West Coast Web Designs",
+    "email": "info@westcoastwebdesigns.com",
+    "phoneForTel": "503-962-0576",
+    "phoneFormatted": "(503) 962-0576",
+    "address": {
+        "lineOne": "4889 Tate Ave N",
+        "city": "Keizer",
+        "state": "OR",
+        "zip": "97303",
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/",
+        "instagram": "https://www.instagram.com/",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://www.westcoastwebdesigns.com"
 };
